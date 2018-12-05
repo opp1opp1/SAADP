0a21d3bd3f01623378ab0161975b9ebce4477231
boshshieh <opp1opp1@yahoo.com.tw>
2018-12-06 02:26:19 +0800

    Rebuild 此專案
    新增漢堡類 裡面目前有一項卡啦雞腿堡
    新增結帳按鈕(目前功能等於清除)

---
+++ SAADP/SAADP/Form1.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Price = New System.Windows.Forms.RichTextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Font = New System.Drawing.Font("微軟正黑體", 16.0!)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(1457, 202)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "漢堡類"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(1475, 25)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(417, 863)
+        Me.RichTextBox1.TabIndex = 1
+        Me.RichTextBox1.Text = ""
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(1475, 978)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(417, 51)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Price
+        '
+        Me.Price.BackColor = System.Drawing.SystemColors.Window
+        Me.Price.Font = New System.Drawing.Font("微軟正黑體", 24.0!)
+        Me.Price.ForeColor = System.Drawing.Color.Red
+        Me.Price.Location = New System.Drawing.Point(1475, 894)
+        Me.Price.Name = "Price"
+        Me.Price.ReadOnly = True
+        Me.Price.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Price.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.Price.Size = New System.Drawing.Size(417, 78)
+        Me.Price.TabIndex = 3
+        Me.Price.Text = ""
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(6, 35)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(139, 145)
+        Me.Button2.TabIndex = 0
+        Me.Button2.Text = "卡啦雞腿堡"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1904, 1041)
+        Me.Controls.Add(Me.Price)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Price As RichTextBox
+End Class
---
+++ SAADP/SAADP/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click '按下卡啦雞腿堡' 
+        RichTextBox1.Text = Button2.Text + vbCrLf + RichTextBox1.Text '將卡啦雞腿堡輸入在品項'
+
+        Price.Text = 45 + Val(Price.Text) '輸入價錢 價錢為45 由於Price的文字是字串所以先轉換成數字'
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        RichTextBox1.Text = ""
+        Price.Text = ""
+    End Sub
+End Class
