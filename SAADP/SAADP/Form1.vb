975ef3ab6fbde18f32f839d6fc2f0f7cdab30908
yen861112 <33426923+yen861112@users.noreply.github.com>
2018-12-09 12:46:38 +0800

    改總匯漢堡名字, 蛋餅類完成

--- SAADP/SAADP/Form1.vb
+++ SAADP/SAADP/Form1.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form1
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Burger1.Click '按下卡啦雞腿堡' 
+    Private Sub Burger1_Click(sender As Object, e As EventArgs) Handles Burger1.Click '按下卡啦雞腿堡' 
         RichTextBox1.Text = Burger1.Text + vbCrLf + RichTextBox1.Text '將卡啦雞腿堡輸入在品項'
 
         Price.Text = 45 + Val(Price.Text)  '輸入價錢 價錢為45 由於Price的文字是字串所以先轉換成數字'
@@ -39,6 +39,47 @@
         RichTextBox1.Text = Burger8.Text + vbCrLf + RichTextBox1.Text '將總匯蛋堡輸入在品項'
 
         Price.Text = 40 + Val(Price.Text) '輸入價錢 價錢為40 由於Price的文字是字串所以先轉換成數字'
+    End Sub
+
+    Private Sub Eggroll1_Click(sender As Object, e As EventArgs) Handles Eggroll1.Click '按下卡啦雞蛋餅'
+        RichTextBox1.Text = Eggroll1.Text + vbCrLf + RichTextBox1.Text '將卡啦雞蛋餅輸入在品項'
+
+        Price.Text = 40 + Val(Price.Text) '輸入價錢 價錢為40 由於Price的文字是字串所以先轉換成數字'
+    End Sub
+    Private Sub Eggroll2_Click(sender As Object, e As EventArgs) Handles Eggroll2.Click '按下玉米蛋餅'
+        RichTextBox1.Text = Eggroll2.Text + vbCrLf + RichTextBox1.Text '將玉米蛋餅輸入在品項'
+
+        Price.Text = 20 + Val(Price.Text) '輸入價錢 價錢為20 由於Price的文字是字串所以先轉換成數字'
+    End Sub
+    Private Sub Eggroll3_Click(sender As Object, e As EventArgs) Handles Eggroll3.Click '按下鮪魚蛋餅'
+        RichTextBox1.Text = Eggroll3.Text + vbCrLf + RichTextBox1.Text '將鮪魚蛋餅輸入在品項'
+
+        Price.Text = 20 + Val(Price.Text) '輸入價錢 價錢為20 由於Price的文字是字串所以先轉換成數字'
+    End Sub
+    Private Sub Eggroll4_Click(sender As Object, e As EventArgs) Handles Eggroll4.Click '按下豬肉蛋餅'
+        RichTextBox1.Text = Eggroll4.Text + vbCrLf + RichTextBox1.Text '將豬肉蛋餅輸入在品項'
+
+        Price.Text = 25 + Val(Price.Text) '輸入價錢 價錢為25 由於Price的文字是字串所以先轉換成數字'
+    End Sub
+    Private Sub Eggroll5_Click(sender As Object, e As EventArgs) Handles Eggroll5.Click '按下烤肉蛋餅'
+        RichTextBox1.Text = Eggroll5.Text + vbCrLf + RichTextBox1.Text '將烤肉蛋餅輸入在品項'
+
+        Price.Text = 30 + Val(Price.Text) '輸入價錢 價錢為30 由於Price的文字是字串所以先轉換成數字'
+    End Sub
+    Private Sub Eggroll6_Click(sender As Object, e As EventArgs) Handles Eggroll6.Click '按下香雞蛋餅'
+        RichTextBox1.Text = Eggroll6.Text + vbCrLf + RichTextBox1.Text '將香雞蛋餅輸入在品項'
+
+        Price.Text = 30 + Val(Price.Text) '輸入價錢 價錢為30 由於Price的文字是字串所以先轉換成數字'
+    End Sub
+    Private Sub Eggroll7_Click(sender As Object, e As EventArgs) Handles Eggroll7.Click '按下牛肉蛋餅'
+        RichTextBox1.Text = Eggroll7.Text + vbCrLf + RichTextBox1.Text '將牛肉蛋餅輸入在品項'
+
+        Price.Text = 30 + Val(Price.Text) '輸入價錢 價錢為30 由於Price的文字是字串所以先轉換成數字'
+    End Sub
+    Private Sub Eggroll8_Click(sender As Object, e As EventArgs) Handles Eggroll8.Click '按下泡菜蛋餅'
+        RichTextBox1.Text = Eggroll8.Text + vbCrLf + RichTextBox1.Text '將泡菜蛋餅輸入在品項'
+
+        Price.Text = 20 + Val(Price.Text) '輸入價錢 價錢為20 由於Price的文字是字串所以先轉換成數字'
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click '按下結帳按鈕'
@@ -117,5 +158,9 @@
         RichTextBox1.Text = "套餐折價" + vbCrLf + RichTextBox1.Text
 
         Price.Text = -5 + Val(Price.Text)
+    End Sub
+
+    Private Sub GroupBox4_Enter(sender As Object, e As EventArgs) Handles GroupBox4.Enter
+
     End Sub
 End Class
