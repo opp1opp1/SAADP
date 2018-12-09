52c213f5f9b333457742894cf652c5535f0765e2
Kevinkang0211 <45477381+Kevinkang0211@users.noreply.github.com>
2018-12-09 12:17:32 +0800

    外帶內用鍵換位置

--- SAADP/SAADP/Form1.Designer.vb
+++ SAADP/SAADP/Form1.Designer.vb
@@ -66,98 +66,110 @@ Partial Class Form1
         Me.GroupBox1.Controls.Add(Me.Burger2)
         Me.GroupBox1.Controls.Add(Me.Burger1)
         Me.GroupBox1.Font = New System.Drawing.Font("微軟正黑體", 16.0!)
-        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Location = New System.Drawing.Point(16, 15)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(1216, 202)
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox1.Size = New System.Drawing.Size(1621, 252)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "漢堡類"
         '
         'Burger8
         '
-        Me.Burger8.Location = New System.Drawing.Point(1063, 140)
+        Me.Burger8.Location = New System.Drawing.Point(1417, 175)
+        Me.Burger8.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Burger8.Name = "Burger8"
-        Me.Burger8.Size = New System.Drawing.Size(145, 40)
+        Me.Burger8.Size = New System.Drawing.Size(193, 50)
         Me.Burger8.TabIndex = 7
         Me.Burger8.Text = "豬肉蛋堡"
         Me.Burger8.UseVisualStyleBackColor = True
         '
         'Burger7
         '
-        Me.Burger7.Location = New System.Drawing.Point(912, 140)
+        Me.Burger7.Location = New System.Drawing.Point(1216, 175)
+        Me.Burger7.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Burger7.Name = "Burger7"
-        Me.Burger7.Size = New System.Drawing.Size(145, 40)
+        Me.Burger7.Size = New System.Drawing.Size(193, 50)
         Me.Burger7.TabIndex = 6
         Me.Burger7.Text = "牛肉蛋堡"
         Me.Burger7.UseVisualStyleBackColor = True
         '
         'Burger6
         '
-        Me.Burger6.Location = New System.Drawing.Point(761, 140)
+        Me.Burger6.Location = New System.Drawing.Point(1015, 175)
+        Me.Burger6.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Burger6.Name = "Burger6"
-        Me.Burger6.Size = New System.Drawing.Size(145, 40)
+        Me.Burger6.Size = New System.Drawing.Size(193, 50)
         Me.Burger6.TabIndex = 5
         Me.Burger6.Text = "香雞蛋堡"
         Me.Burger6.UseVisualStyleBackColor = True
         '
         'Burger5
         '
-        Me.Burger5.Location = New System.Drawing.Point(610, 140)
+        Me.Burger5.Location = New System.Drawing.Point(813, 175)
+        Me.Burger5.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Burger5.Name = "Burger5"
-        Me.Burger5.Size = New System.Drawing.Size(145, 40)
+        Me.Burger5.Size = New System.Drawing.Size(193, 50)
         Me.Burger5.TabIndex = 4
         Me.Burger5.Text = "烤肉蛋堡"
         Me.Burger5.UseVisualStyleBackColor = True
         '
         'Burger4
         '
-        Me.Burger4.Location = New System.Drawing.Point(459, 140)
+        Me.Burger4.Location = New System.Drawing.Point(612, 175)
+        Me.Burger4.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Burger4.Name = "Burger4"
-        Me.Burger4.Size = New System.Drawing.Size(145, 40)
+        Me.Burger4.Size = New System.Drawing.Size(193, 50)
         Me.Burger4.TabIndex = 3
         Me.Burger4.Text = "豬肉蛋堡"
         Me.Burger4.UseVisualStyleBackColor = True
         '
         'Burger3
         '
-        Me.Burger3.Location = New System.Drawing.Point(308, 140)
+        Me.Burger3.Location = New System.Drawing.Point(411, 175)
+        Me.Burger3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Burger3.Name = "Burger3"
-        Me.Burger3.Size = New System.Drawing.Size(145, 40)
+        Me.Burger3.Size = New System.Drawing.Size(193, 50)
         Me.Burger3.TabIndex = 2
         Me.Burger3.Text = "肉鬆蛋堡"
         Me.Burger3.UseVisualStyleBackColor = True
         '
         'Burger2
         '
-        Me.Burger2.Location = New System.Drawing.Point(157, 140)
+        Me.Burger2.Location = New System.Drawing.Point(209, 175)
+        Me.Burger2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Burger2.Name = "Burger2"
-        Me.Burger2.Size = New System.Drawing.Size(145, 40)
+        Me.Burger2.Size = New System.Drawing.Size(193, 50)
         Me.Burger2.TabIndex = 1
         Me.Burger2.Text = "玉米蛋堡"
         Me.Burger2.UseVisualStyleBackColor = True
         '
         'Burger1
         '
-        Me.Burger1.Location = New System.Drawing.Point(6, 140)
+        Me.Burger1.Location = New System.Drawing.Point(8, 175)
+        Me.Burger1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Burger1.Name = "Burger1"
-        Me.Burger1.Size = New System.Drawing.Size(145, 40)
+        Me.Burger1.Size = New System.Drawing.Size(193, 50)
         Me.Burger1.TabIndex = 0
         Me.Burger1.Text = "卡啦雞腿堡"
         Me.Burger1.UseVisualStyleBackColor = True
         '
         'RichTextBox1
         '
-        Me.RichTextBox1.Location = New System.Drawing.Point(1475, 25)
+        Me.RichTextBox1.Location = New System.Drawing.Point(1967, 31)
+        Me.RichTextBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.RichTextBox1.Name = "RichTextBox1"
-        Me.RichTextBox1.Size = New System.Drawing.Size(417, 863)
+        Me.RichTextBox1.Size = New System.Drawing.Size(555, 1078)
         Me.RichTextBox1.TabIndex = 1
         Me.RichTextBox1.Text = ""
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(1475, 978)
+        Me.Button1.Location = New System.Drawing.Point(1967, 1222)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(417, 51)
+        Me.Button1.Size = New System.Drawing.Size(556, 64)
         Me.Button1.TabIndex = 2
         Me.Button1.Text = "結帳"
         Me.Button1.UseVisualStyleBackColor = True
@@ -167,12 +179,13 @@ Partial Class Form1
         Me.Price.BackColor = System.Drawing.SystemColors.Window
         Me.Price.Font = New System.Drawing.Font("微軟正黑體", 24.0!)
         Me.Price.ForeColor = System.Drawing.Color.Red
-        Me.Price.Location = New System.Drawing.Point(1475, 894)
+        Me.Price.Location = New System.Drawing.Point(1967, 1118)
+        Me.Price.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Price.Name = "Price"
         Me.Price.ReadOnly = True
         Me.Price.RightToLeft = System.Windows.Forms.RightToLeft.Yes
         Me.Price.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
-        Me.Price.Size = New System.Drawing.Size(417, 78)
+        Me.Price.Size = New System.Drawing.Size(555, 96)
         Me.Price.TabIndex = 3
         Me.Price.Text = ""
         '
@@ -184,54 +197,61 @@ Partial Class Form1
         Me.GroupBox2.Controls.Add(Me.Drink2)
         Me.GroupBox2.Controls.Add(Me.Drink1)
         Me.GroupBox2.Font = New System.Drawing.Font("微軟正黑體", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
-        Me.GroupBox2.Location = New System.Drawing.Point(12, 220)
+        Me.GroupBox2.Location = New System.Drawing.Point(16, 275)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(766, 197)
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox2.Size = New System.Drawing.Size(1021, 246)
         Me.GroupBox2.TabIndex = 4
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "飲料類"
         '
         'Drink5
         '
-        Me.Drink5.Location = New System.Drawing.Point(610, 152)
+        Me.Drink5.Location = New System.Drawing.Point(813, 190)
+        Me.Drink5.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Drink5.Name = "Drink5"
-        Me.Drink5.Size = New System.Drawing.Size(145, 39)
+        Me.Drink5.Size = New System.Drawing.Size(193, 49)
         Me.Drink5.TabIndex = 8
         Me.Drink5.Text = "豆漿"
         Me.Drink5.UseVisualStyleBackColor = True
         '
         'Drink4
         '
-        Me.Drink4.Location = New System.Drawing.Point(459, 152)
+        Me.Drink4.Location = New System.Drawing.Point(612, 190)
+        Me.Drink4.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Drink4.Name = "Drink4"
-        Me.Drink4.Size = New System.Drawing.Size(145, 39)
+        Me.Drink4.Size = New System.Drawing.Size(193, 49)
         Me.Drink4.TabIndex = 7
         Me.Drink4.Text = "柳橙汁"
         Me.Drink4.UseVisualStyleBackColor = True
         '
         'Drink3
         '
-        Me.Drink3.Location = New System.Drawing.Point(308, 152)
+        Me.Drink3.Location = New System.Drawing.Point(411, 190)
+        Me.Drink3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Drink3.Name = "Drink3"
-        Me.Drink3.Size = New System.Drawing.Size(145, 40)
+        Me.Drink3.Size = New System.Drawing.Size(193, 50)
         Me.Drink3.TabIndex = 6
         Me.Drink3.Text = "綠茶"
         Me.Drink3.UseVisualStyleBackColor = True
         '
         'Drink2
         '
-        Me.Drink2.Location = New System.Drawing.Point(157, 152)
+        Me.Drink2.Location = New System.Drawing.Point(209, 190)
+        Me.Drink2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Drink2.Name = "Drink2"
-        Me.Drink2.Size = New System.Drawing.Size(145, 40)
+        Me.Drink2.Size = New System.Drawing.Size(193, 50)
         Me.Drink2.TabIndex = 5
         Me.Drink2.Text = "紅茶"
         Me.Drink2.UseVisualStyleBackColor = True
         '
         'Drink1
         '
-        Me.Drink1.Location = New System.Drawing.Point(6, 152)
+        Me.Drink1.Location = New System.Drawing.Point(8, 190)
+        Me.Drink1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Drink1.Name = "Drink1"
-        Me.Drink1.Size = New System.Drawing.Size(145, 40)
+        Me.Drink1.Size = New System.Drawing.Size(193, 50)
         Me.Drink1.TabIndex = 0
         Me.Drink1.Text = "奶茶"
         Me.Drink1.UseVisualStyleBackColor = True
@@ -239,9 +259,10 @@ Partial Class Form1
         'Button2
         '
         Me.Button2.Font = New System.Drawing.Font("微軟正黑體", 24.0!)
-        Me.Button2.Location = New System.Drawing.Point(1235, 25)
+        Me.Button2.Location = New System.Drawing.Point(1647, 31)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(234, 189)
+        Me.Button2.Size = New System.Drawing.Size(312, 236)
         Me.Button2.TabIndex = 5
         Me.Button2.Text = "套餐飲料折抵"
         Me.Button2.UseVisualStyleBackColor = True
@@ -249,9 +270,10 @@ Partial Class Form1
         'Admin1
         '
         Me.Admin1.Font = New System.Drawing.Font("微軟正黑體", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
-        Me.Admin1.Location = New System.Drawing.Point(21, 41)
+        Me.Admin1.Location = New System.Drawing.Point(28, 51)
+        Me.Admin1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Admin1.Name = "Admin1"
-        Me.Admin1.Size = New System.Drawing.Size(80, 80)
+        Me.Admin1.Size = New System.Drawing.Size(107, 100)
         Me.Admin1.TabIndex = 6
         Me.Admin1.Text = "清除菜單"
         Me.Admin1.UseVisualStyleBackColor = True
@@ -259,9 +281,10 @@ Partial Class Form1
         'Admin2
         '
         Me.Admin2.Font = New System.Drawing.Font("微軟正黑體", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
-        Me.Admin2.Location = New System.Drawing.Point(127, 41)
+        Me.Admin2.Location = New System.Drawing.Point(169, 51)
+        Me.Admin2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Admin2.Name = "Admin2"
-        Me.Admin2.Size = New System.Drawing.Size(80, 80)
+        Me.Admin2.Size = New System.Drawing.Size(107, 100)
         Me.Admin2.TabIndex = 7
         Me.Admin2.Text = "Promo Item"
         Me.Admin2.UseVisualStyleBackColor = True
@@ -269,9 +292,10 @@ Partial Class Form1
         'Admin3
         '
         Me.Admin3.Font = New System.Drawing.Font("微軟正黑體", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
-        Me.Admin3.Location = New System.Drawing.Point(21, 127)
+        Me.Admin3.Location = New System.Drawing.Point(28, 159)
+        Me.Admin3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Admin3.Name = "Admin3"
-        Me.Admin3.Size = New System.Drawing.Size(80, 80)
+        Me.Admin3.Size = New System.Drawing.Size(107, 100)
         Me.Admin3.TabIndex = 8
         Me.Admin3.Text = "領班選項"
         Me.Admin3.UseVisualStyleBackColor = True
@@ -279,9 +303,10 @@ Partial Class Form1
         'Admin4
         '
         Me.Admin4.Font = New System.Drawing.Font("微軟正黑體", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
-        Me.Admin4.Location = New System.Drawing.Point(127, 127)
+        Me.Admin4.Location = New System.Drawing.Point(169, 159)
+        Me.Admin4.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Admin4.Name = "Admin4"
-        Me.Admin4.Size = New System.Drawing.Size(80, 80)
+        Me.Admin4.Size = New System.Drawing.Size(107, 100)
         Me.Admin4.TabIndex = 9
         Me.Admin4.Text = "查看上筆"
         Me.Admin4.UseVisualStyleBackColor = True
@@ -289,9 +314,10 @@ Partial Class Form1
         'Admin5
         '
         Me.Admin5.Font = New System.Drawing.Font("微軟正黑體", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
-        Me.Admin5.Location = New System.Drawing.Point(21, 213)
+        Me.Admin5.Location = New System.Drawing.Point(28, 266)
+        Me.Admin5.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Admin5.Name = "Admin5"
-        Me.Admin5.Size = New System.Drawing.Size(80, 80)
+        Me.Admin5.Size = New System.Drawing.Size(107, 100)
         Me.Admin5.TabIndex = 10
         Me.Admin5.Text = "員工餐"
         Me.Admin5.UseVisualStyleBackColor = True
@@ -299,9 +325,10 @@ Partial Class Form1
         'Admin6
         '
         Me.Admin6.Font = New System.Drawing.Font("微軟正黑體", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
-        Me.Admin6.Location = New System.Drawing.Point(127, 213)
+        Me.Admin6.Location = New System.Drawing.Point(169, 266)
+        Me.Admin6.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Admin6.Name = "Admin6"
-        Me.Admin6.Size = New System.Drawing.Size(80, 80)
+        Me.Admin6.Size = New System.Drawing.Size(107, 100)
         Me.Admin6.TabIndex = 11
         Me.Admin6.Text = "On9"
         Me.Admin6.UseVisualStyleBackColor = True
@@ -309,9 +336,10 @@ Partial Class Form1
         'Admin7
         '
         Me.Admin7.Font = New System.Drawing.Font("微軟正黑體", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
-        Me.Admin7.Location = New System.Drawing.Point(21, 299)
+        Me.Admin7.Location = New System.Drawing.Point(169, 374)
+        Me.Admin7.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Admin7.Name = "Admin7"
-        Me.Admin7.Size = New System.Drawing.Size(80, 80)
+        Me.Admin7.Size = New System.Drawing.Size(107, 100)
         Me.Admin7.TabIndex = 12
         Me.Admin7.Text = "外帶"
         Me.Admin7.UseVisualStyleBackColor = True
@@ -319,9 +347,10 @@ Partial Class Form1
         'Admin8
         '
         Me.Admin8.Font = New System.Drawing.Font("微軟正黑體", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
-        Me.Admin8.Location = New System.Drawing.Point(127, 299)
+        Me.Admin8.Location = New System.Drawing.Point(28, 374)
+        Me.Admin8.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Admin8.Name = "Admin8"
-        Me.Admin8.Size = New System.Drawing.Size(80, 80)
+        Me.Admin8.Size = New System.Drawing.Size(107, 100)
         Me.Admin8.TabIndex = 13
         Me.Admin8.Text = "內用"
         Me.Admin8.UseVisualStyleBackColor = True
@@ -337,18 +366,20 @@ Partial Class Form1
         Me.GroupBox3.Controls.Add(Me.Admin6)
         Me.GroupBox3.Controls.Add(Me.Admin5)
         Me.GroupBox3.Font = New System.Drawing.Font("微軟正黑體", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
-        Me.GroupBox3.Location = New System.Drawing.Point(1247, 220)
+        Me.GroupBox3.Location = New System.Drawing.Point(1663, 275)
+        Me.GroupBox3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.GroupBox3.Name = "GroupBox3"
-        Me.GroupBox3.Size = New System.Drawing.Size(222, 397)
+        Me.GroupBox3.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox3.Size = New System.Drawing.Size(296, 496)
         Me.GroupBox3.TabIndex = 15
         Me.GroupBox3.TabStop = False
         Me.GroupBox3.Text = "員工選項"
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1904, 1041)
+        Me.ClientSize = New System.Drawing.Size(1924, 1055)
         Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.GroupBox2)
@@ -356,8 +387,9 @@ Partial Class Form1
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.RichTextBox1)
         Me.Controls.Add(Me.GroupBox1)
-        Me.MaximumSize = New System.Drawing.Size(1920, 1080)
-        Me.MinimumSize = New System.Drawing.Size(1856, 1078)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.MaximumSize = New System.Drawing.Size(2554, 1338)
+        Me.MinimumSize = New System.Drawing.Size(1918, 1028)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.GroupBox1.ResumeLayout(False)
